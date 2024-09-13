ee85ad6de4098b18ca4a1797b6c7c056bdda2446
chanekarayush <chanekarayush@gmail.com>
2024-09-13 11:38:11 +0530

    JDBC connectivity

--- Assignment8_JDBC/JDBC/src/App.java
+++ Assignment8_JDBC/JDBC/src/App.java
@@ -1,5 +1,11 @@
+import java.sql.*;
+import com.mysql.*;
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        String url = "jdbc:mysql://127.0.0.1:3306/assignment_8_dbms";
+        
     }
 }
